a2cc33aca891e5ceacc96a53f9a1f5a9c6469a96
akshansha <akshanshagohil1995@gmail.com>
2019-09-17 17:25:11 +0530

    Add project files.

---
+++ Identity/Services/EFLoginService.cs
@@ -0,0 +1,41 @@
+﻿using Identity.Model;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.Services
+{
+    public class EFLoginService : ILoginService<ApplicationUser>
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public EFLoginService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public async Task<ApplicationUser> FindByUsername(string user)
+        {
+            return await _userManager.FindByEmailAsync(user);
+        }
+
+        public async Task<bool> ValidateCredentials(ApplicationUser user, string password)
+        {
+            return await _userManager.CheckPasswordAsync(user, password);
+        }
+
+        public Task SignIn(ApplicationUser user)
+        {
+            return _signInManager.SignInAsync(user, true);
+        }
+
+        public Task SignInAsync(ApplicationUser user, AuthenticationProperties properties, string authenticationMethod = null)
+        {
+            return _signInManager.SignInAsync(user, properties, authenticationMethod);
+        }
+    }
+}
---
+++ Identity/Startup.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Identity.Data;
+using Identity.Model;
+using Identity.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Identity
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<ApplicationDBContext>(options =>
+                                        options.UseSqlServer(Configuration.GetConnectionString("marvinUserDBConnectionString")));
+
+            services.AddScoped<ILoginService<ApplicationUser>, EFLoginService>();
+            var connectionString = Configuration["ConnectionString"];
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+
+            services.AddIdentityServer(x =>
+            {
+                x.IssuerUri = "null";
+
+            })
+            .AddDeveloperSigningCredential()
+            .AddAspNetIdentity<ApplicationUser>()
+            .AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString,
+                    sql => sql.MigrationsAssembly(migrationsAssembly));
+                //sqlServerOptionsAction: sqlOptions =>
+                //{
+                //    sqlOptions.MigrationsAssembly(migrationsAssembly);
+
+                //    sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                //});
+            })
+            .AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString,
+                    sql => sql.MigrationsAssembly(migrationsAssembly));
+
+
+                //sqlServerOptionsAction: sqlOptions =>
+                //{
+                //    sqlOptions.MigrationsAssembly(migrationsAssembly);
+                //    //Configuring Connection Resiliency: https://docs.microsoft.com/en-us/ef/core/miscellaneous/connection-resiliency 
+                //    sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                //});
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseIdentityServer();
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
