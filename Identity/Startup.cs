b3531ee5ebc833af327ee193d4fa316ce9d6f407
akshansha <akshanshagohil1995@gmail.com>
2019-09-23 15:03:53 +0530

    set logo

--- Identity/Startup.cs
+++ Identity/Startup.cs
@@ -90,6 +90,7 @@ namespace Identity
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseStaticFiles();
             app.UseIdentityServer();
 
             app.UseHttpsRedirection();
